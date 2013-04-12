d6bfc44ca24cd7cd15af08e5fec4699ef0c21bd8
Apprentice <apprentice@dbc02.local>
2013-04-12 12:46:10 -0700

    add user controller, add new and edit page

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,13 @@
+#if there it's a redirection, save error message in session. else pass message
+#by assigning it to @errors
+before do
+  @errors = session[:errors]
+  session[:errors] = nil if @errors
+  @messages = session[:messages]
+  session[:messages] = nil if @errors
+end
+
+
 get '/' do
   # Look in app/views/index.erb
   erb :index
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,61 @@
+namespace '/users' do
+
+  get '/new' do
+    erb :"users/new"
+  end
+
+  post '/' do
+    @user = User.new(params[:user])
+
+    if @user.save
+      #remember to login user here #work on this when working on session
+      redirect "/users/#{@user.id}"
+    else
+      session[:errors] = "We love you but we failed to create an account for you. Please try again!"
+      redirect '/'
+    end
+  end
+
+  get '/:id' do
+    user_id = params[:id]
+
+    @user = User.find_by_id(user_id)
+    if @user
+      erb :"users/show"
+    else
+      session[:errors] = "Couldn't find user with ID #{user_id}"
+      redirect '/'
+    end
+  end
+
+  get '/:id/edit' do
+    user_id = params[:id]
+
+    @user = User.find_by_id(user_id)
+    if @user
+      erb :"users/edit"
+    else
+      session[:errors] = "No such user!"
+      redirect '/'
+    end
+  end
+
+  put '/' do
+    user_id = params[:user_id]
+
+    @user = User.find_by_id(user_id)
+    if @user
+      @user.update_attributes(params[:user])
+      redirect "/users/#{user_id}"
+    else
+      session[:errors] = "Unauthorized yo!"
+      redirect '/'
+    end
+  end
+
+  delete '/:id' do
+    User.delete(params[:id])
+    session[:messages] = "User deleted"
+    redirect '/'
+  end
+end
