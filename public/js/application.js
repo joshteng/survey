9abe106b0e013dfbed75efda6783e938717a6068
Apprentice <apprentice@dbc01.local>
2013-04-12 20:52:40 -0700

    new survey js

--- public/js/application.js
+++ public/js/application.js
@@ -13,7 +13,20 @@ $(document).ready(function() {
   //     window.location.href('/surveys')
   //   })
   // })
+  var question = $('.question').clone();
+  var choice = $('.choice').clone();
 
-  $('form').on('click')
+
+  $('form').on('click', 'button[name=add_choice]', function(e) {
+    e.preventDefault();
+    $(this).closest('.choice').after(choice.clone());
+    $(this).remove();
+  })
+
+  $('form').on('click', 'button[name=add_question]', function(e) {
+    e.preventDefault();
+    $(this).closest('.question').after(question.clone());
+    $(this).remove();
+  })  
 
 });
